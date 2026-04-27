e4e3e7d36c667852bcde515d7c20f4b096eac94c
FlaviaTironi <flaviasst@al.insper.edu.br>
2026-04-27 15:17:23 -0300

    exercício 1

---
+++ funcoes.py
@@ -0,0 +1,10 @@
+import random
+
+def rolar_dados(n):
+    lista = []
+    i = 0
+    while i < n:
+        numero = random.randint(1,6)
+        lista.append(numero)
+        i += 1
+    return lista
